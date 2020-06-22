71ee289d32881d69934234d5abdb826cfdf8912c
Jeffrey Flynn <jeffrey@livefeather.com>
2020-06-22 16:57:54 -0400

    Updates

---
+++ precache-manifest.95f703c82352e9df4f4b29e84feae09a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f76640819d73c999a64021c1d174ee31",
+    "url": "/QR_Scanning/index.html"
+  },
+  {
+    "revision": "8804d477109f8c32091f",
+    "url": "/QR_Scanning/static/css/main.483d5f64.chunk.css"
+  },
+  {
+    "revision": "ff33f2e3e617144b8dbd",
+    "url": "/QR_Scanning/static/js/2.5e949b3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QR_Scanning/static/js/2.5e949b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8804d477109f8c32091f",
+    "url": "/QR_Scanning/static/js/main.7199edcf.chunk.js"
+  },
+  {
+    "revision": "b555a5e6f83e21aa8866",
+    "url": "/QR_Scanning/static/js/runtime-main.ced6aa04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/QR_Scanning/static/media/logo.5d5d9eef.svg"
+  }
+]);
