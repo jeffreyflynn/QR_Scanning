72ff38af13841023a94a7a140914e81a8e236bae
Jeffrey Flynn <jeffrey@livefeather.com>
2020-06-25 13:47:48 -0400

    Updates

--- precache-manifest.f3380706b932b17db30decbf6b067d0c.js
+++ precache-manifest.f3380706b932b17db30decbf6b067d0c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c5e8dacf08b51d7a7af797dac1af549",
+    "revision": "4791ac08924ea39a56be882c918b00f8",
     "url": "/QR_Scanning/index.html"
   },
   {
-    "revision": "9a39c8b31b09d2f893ac",
+    "revision": "3e70c28757b2629d92f6",
     "url": "/QR_Scanning/static/css/main.e1ccaa11.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/QR_Scanning/static/js/2.29b9a009.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9a39c8b31b09d2f893ac",
-    "url": "/QR_Scanning/static/js/main.d84dec8d.chunk.js"
+    "revision": "3e70c28757b2629d92f6",
+    "url": "/QR_Scanning/static/js/main.b1b52ec2.chunk.js"
   },
   {
     "revision": "b555a5e6f83e21aa8866",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/QR_Scanning/precache-manifest.86bd182086a1d3a8c3f1608f0c9dcbdd.js"
+  "/QR_Scanning/precache-manifest.f3380706b932b17db30decbf6b067d0c.js"
 );
 
 self.addEventListener('message', (event) => {
